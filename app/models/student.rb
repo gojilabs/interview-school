5d87aefb795a30df942a17cde11ecf6d5e3f8e83
Brian Nguyen <brian@mindrglobal.com>
2022-08-11 09:30:39 +1000

    Make enrolled_sections less confusing

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,6 @@
 class Student < ApplicationRecord
-
-  has_many :enrolled_sections, class_name: 'StudentSection'
+  has_many :student_sections
+  has_many :enrolled_sections, source: :section, through: :student_sections
 
   def first_and_last_name
     "#{first_name} #{last_name}"
